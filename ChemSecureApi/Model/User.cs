ff236b0ea17366c200ac058d02e8f21a8e19705f
SergiAlbalat <sergi.albalat.7e8@itb.cat>
2025-05-19 09:49:57 +0200

    #3.feature(api): Models Creats

---
+++ ChemSecureApi/Model/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ChemSecureApi.Model
+{
+    public class User : IdentityUser
+    {
+        public string Address {  get; set; }
+        public List<Tank> Tanks { get; set; }
+    }
+}
