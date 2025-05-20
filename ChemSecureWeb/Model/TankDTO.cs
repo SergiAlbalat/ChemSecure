f45b4125231902fdbf5739dfbe576589eb593294
GuillemSoto <guillem.soto.7e8@itb.cat>
2025-05-20 09:53:17 +0200

    #5.Feature TankDTO  created

---
+++ ChemSecureWeb/Model/TankDTO.cs
@@ -0,0 +1,26 @@
+﻿namespace ChemSecureWeb.Model
+{
+    public enum residusType { HalogenatedSolvents, NonHalogenatedSolvents, AqueousSolutions, Acids, Oils };
+    public class TankDTO
+    {
+        public int Id { get; set; }
+        public double CurrentVolume { get; set; }
+        public double Capacity { get; set; }
+        public residusType Type { get; set; }
+        public double Percentage
+        {
+            get
+            {
+                return CalculatePercentage();
+            }
+            set
+            {
+
+            }
+        }
+        public double CalculatePercentage()
+        {
+            return CurrentVolume / Capacity * 100;
+        }
+    }
+}
