b015810eca1dd1d2453f88a28198efd32e3f407e
SergiAlbalat <sergi.albalat.7e8@itb.cat>
2025-05-20 09:56:29 +0200

    #27.Feature(api): Migrations to the database applied

--- ChemSecureApi/Data/AppDbContext.cs
+++ ChemSecureApi/Data/AppDbContext.cs
@@ -12,5 +12,18 @@ namespace ChemSecureApi.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Tank>()
+                .HasOne(t => t.Client)
+                .WithMany(u => u.Tanks)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<User>()
+                .HasMany(u => u.Tanks)
+                .WithOne(t => t.Client)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
