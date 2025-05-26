727c02a6c195a52408b2c87ec17b008f04f9c13c
Sergio Vegas <sergio.vegas.7e8@itb.cat>
2025-05-26 10:26:33 +0200

    #80 . TankPage Done and functional

--- ChemSecureWeb/Model/TankDTO.cs
+++ ChemSecureWeb/Model/TankDTO.cs
@@ -1,4 +1,4 @@
-﻿namespace ChemSecureWeb.Model
+namespace ChemSecureWeb.Model
 {
     public enum residusType { HalogenatedSolvents, NonHalogenatedSolvents, AqueousSolutions, Acids, Oils };
     public class TankDTO
@@ -7,6 +7,8 @@
         public double CurrentVolume { get; set; }
         public double Capacity { get; set; }
         public residusType Type { get; set; }
+        public string UserId { get; set; } = string.Empty;
+        public string UserEmail { get; set; } = string.Empty;
         public double Percentage
         {
             get
